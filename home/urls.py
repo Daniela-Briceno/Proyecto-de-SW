30ca453d8db701fb61816b1315d4aa7854ff0587
Daniela-Briceno <daniela.bricenop@userena.cl>
2024-05-21 17:30:27 -0400

    Segundo commit

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.my_view, name='my-view'),
+    path('', views.home, name='body'),
+    path('datos_historicos.html', views.datos, name='datos'),
+    path('estaciones.html', views.estaciones, name='estaciones'),
+    path('pronostico_mp10.html', views.pronostico, name='pronostico'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 
-def my_view(request):
+def home(request):
     return render(request, 'body.html')
+
+def datos(request):
+    return render(request, 'datos_historicos.html')
+
+def estaciones(request):
+    return render(request, 'estaciones.html')
+
+def pronostico(request):
+    return render(request, 'pronostico_mp10.html')
+
