dcfcfa50fdecd02c36d8a9d947cbebcb0be2df63
Daniela-Briceno <daniela.bricenop@userena.cl>
2024-05-29 20:35:39 -0400

    arreglos y predicción

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('datos_historicos.html', views.datos, name='datos'),
     path('estaciones.html', views.estaciones, name='estaciones'),
     path('pronostico_mp10.html', views.pronostico, name='pronostico'),
+    path('proyecto.html', views.proyecto, name='proyecto'),
+    path('mp10.html', views.mp10, name='mp10'),
+    path('quienesomos.html', views.quienesomos, name='quienesomos'),
 ]
--- home/views.py
+++ home/views.py
@@ -17,4 +17,13 @@ def estaciones(request):
 def pronostico(request):
     return render(request, 'pronostico_mp10.html')
 
+def proyecto(request):
+    return render(request, 'proyecto.html')
+
+def mp10(request):
+    return render(request, 'mp10.html')
+
+def quienesomos(request):
+    return render(request, 'quienesomos.html')
+
 
