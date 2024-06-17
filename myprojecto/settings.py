846b1c958667512c69a60d7d8f02978e5ccb34f9
Daniela-Briceno <daniela.bricenop@userena.cl>
2024-06-17 15:44:09 -0400

    Actualizacion

--- myprojecto/settings.py
+++ myprojecto/settings.py
@@ -118,8 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    '/path/to/other/static/directory/',
+    os.path.join(BASE_DIR, 'static')
 ]
 
 
